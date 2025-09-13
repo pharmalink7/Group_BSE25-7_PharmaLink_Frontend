5fd576a0b5e2ef346b3aa4a38bdfcecc2509d3e0
CYIZA NDOLI <cyndoli34@gmail.com>
2025-09-13 21:14:32 +0300

    Renaming a file

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../styles/HomePage.css";
+import "../styles/Homepage.css";
 
 export default function HomePage() {
   return (
