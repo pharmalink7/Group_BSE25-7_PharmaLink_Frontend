64bea9e16b5d7e09944b833dca5c61b8fa582c79
Yusuf <mulindway6@gmail.com>
2025-09-19 10:25:19 +0300

    Corrected AuthProvider importation naming

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
+import { AuthProvider } from './contexts/authContext';
 
 import ProtectedRoute from './Components/ProtectedRoute';
 import HomePage from './pages/HomePage';
